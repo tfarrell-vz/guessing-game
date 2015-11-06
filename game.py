14b2a44a1952dea14c4f2aebd5503bfd2d04c799
tfarrell-vz <farrell@abiresearch.com>
2015-11-05 21:35:21 -0500

    Spacing

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import random
 
+
 class Game:
     def __init__(self, upper_bound=100):
         self.guesses = []
         self.won = False
-        self.answer = random.randint(1,upper_bound)
+        self.answer = random.randint(1, upper_bound)
 
     def make_guess(self, num):
         self.guesses.append(num)
@@ -19,6 +20,7 @@ def main():
         game.make_guess(int(guess))
 
     print("You won! It only took you %s attempts" % len(game.guesses))
+
 
 if __name__ == '__main__':
     main()
