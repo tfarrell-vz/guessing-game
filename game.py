e268399f5df05cacc538640e9e8c456220f15d94
tfarrell-vz <farrell@abiresearch.com>
2015-11-05 21:14:32 -0500

    Enable a guess, and check the state of the game.

--- game.py
+++ game.py
@@ -5,3 +5,11 @@ class Game:
         self.guesses = []
         self.won = False
         self.answer = random.randint(1,100)
+
+    def make_guess(self, num):
+        self.guesses.append(num)
+        if num == self.answer:
+            self.won = True
+
+    def game_won(self):
+        return self.won
