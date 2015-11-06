fddb69bdff8f724d8554506f38bacfbb54218b33
tfarrell-vz <farrell@abiresearch.com>
2015-11-05 21:33:44 -0500

    A little game loop.

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 import random
 
 class Game:
-    def __init__(self):
+    def __init__(self, upper_bound=100):
         self.guesses = []
         self.won = False
-        self.answer = random.randint(1,100)
+        self.answer = random.randint(1,upper_bound)
 
     def make_guess(self, num):
         self.guesses.append(num)
@@ -13,10 +13,10 @@ class Game:
 
 
 def main():
-    game = Game()
+    game = Game(upper_bound=5)
     while not game.won:
         guess = input(">> Enter a guess: ")
-        game.make_guess(guess)
+        game.make_guess(int(guess))
 
     print("You won! It only took you %s attempts" % len(game.guesses))
 
